288a41cf806c422416b9b82ae894eed8c0e369dd
renesalamanca <rjsalamanca@gmail.com>
2025-09-10 14:48:58 -0400

    Cambios 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,19 +19,52 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Retrieve the ISBN parameter from the request URL and send the corresponding book's details
+    const isbn = req.params.isbn;
+
+    // Convert the books object to an array of its values
+    const booksArray = Object.values(books);
+
+    let bookbyisbn = booksArray.filter(book => book.isbn === isbn);
+    res.send(JSON.stringify(bookbyisbn));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Retrieve the author parameter from the request URL and send the corresponding book's details
+  const author = req.params.author;
+
+  // Convert the books object to an array of its values
+  const booksArray = Object.values(books);
+
+  let booksByAuthor = [];
+
+  booksArray.forEach(book => {
+    if (book["author"] === author) {
+        booksByAuthor.push(book);
+    }
+  });
+  res.send(JSON.stringify(booksByAuthor));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    // Retrieve the author parameter from the request URL and send the corresponding book's details
+    const title = req.params.title;
+  
+    // Convert the books object to an array of its values
+    const booksArray = Object.values(books);
+  
+    let booksByTitle = [];
+  
+    booksArray.forEach(book => {
+      if (book["title"] === title) {
+        booksByTitle.push(book);
+      }
+    });
+    res.send(JSON.stringify(booksByTitle));
 });
 
 //  Get book review
