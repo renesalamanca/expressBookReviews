4fb2a138024235e2097921ad7f80e3aec87343d4
renesalamanca <rjsalamanca@gmail.com>
2025-09-10 15:27:02 -0400

    Cambios 4

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -34,26 +34,6 @@ const authenticatedUser = (username,password)=>{ //returns boolean
     }
 }
 
-// Register a new user
-regd_users.post("/register", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    // Check if both username and password are provided
-    if (username && password) {
-        // Check if the user does not already exist
-        if (!isValid(username)) {
-            // Add the new user to the users array
-            users.push({"username": username, "password": password});
-            return res.status(200).json({message: "User successfully registered. Now you can login"});
-        } else {
-            return res.status(404).json({message: "User already exists!"});
-        }
-    }
-    // Return error if username or password is missing
-    return res.status(404).json({message: "Unable to register user."});
-});
-
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
